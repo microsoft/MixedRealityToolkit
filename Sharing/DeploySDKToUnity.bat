50704cc70484a0b37eef3db5bb729d2d33021f55
Matthew Johnson <mattjo@microsoft.com>
2016-12-08 16:03:16 -0800

    Fix the DeploySDKToUnity script to use the correct environment variable for copying

--- Sharing/DeploySDKToUnity.bat
+++ Sharing/DeploySDKToUnity.bat
@@ -6,33 +6,33 @@ if "x%HoloToolkitUnityRoot%" == "x" goto ErrorNoHoloToolkitUnityTarget
 if not exist "%HoloToolkitUnityRoot%\Assets" goto ErrorHoloToolkitUnityTargetInvalid
 if not exist "%HoloToolkitUnityRoot%\External" goto ErrorHoloToolkitUnityTargetInvalid
 
-xcopy SDK\Client\CSharp\API\* "%UnityRoot%\Assets\HoloToolkit\Sharing\Scripts\SDK\" /S /I /F /H /R /K /Y
+xcopy SDK\Client\CSharp\API\* "%HoloToolkitUnityRoot%\Assets\HoloToolkit\Sharing\Scripts\SDK\" /S /I /F /H /R /K /Y
 
-xcopy SDK\Client\CSharp\bin\Windows\x86\SharingClient.dll "%UnityRoot%\Assets\HoloToolkit\Sharing\Plugins\x86\" /S /I /F /H /R /K /Y
-xcopy SDK\Client\CSharp\bin\Windows\x64\SharingClient.dll "%UnityRoot%\Assets\HoloToolkit\Sharing\Plugins\x64\" /S /I /F /H /R /K /Y
+xcopy SDK\Client\CSharp\bin\Windows\x86\SharingClient.dll "%HoloToolkitUnityRoot%\Assets\HoloToolkit\Sharing\Plugins\x86\" /S /I /F /H /R /K /Y
+xcopy SDK\Client\CSharp\bin\Windows\x64\SharingClient.dll "%HoloToolkitUnityRoot%\Assets\HoloToolkit\Sharing\Plugins\x64\" /S /I /F /H /R /K /Y
 
-xcopy SDK\Client\CSharp\bin\UWP\ARM\SharingClient.dll "%UnityRoot%\Assets\HoloToolkit\Sharing\Plugins\WSA\ARM\" /S /I /F /H /R /K /Y
-xcopy SDK\Client\CSharp\bin\UWP\x86\SharingClient.dll "%UnityRoot%\Assets\HoloToolkit\Sharing\Plugins\WSA\x86\" /S /I /F /H /R /K /Y
-xcopy SDK\Client\CSharp\bin\UWP\x64\SharingClient.dll "%UnityRoot%\Assets\HoloToolkit\Sharing\Plugins\WSA\x64\" /S /I /F /H /R /K /Y
+xcopy SDK\Client\CSharp\bin\UWP\ARM\SharingClient.dll "%HoloToolkitUnityRoot%\Assets\HoloToolkit\Sharing\Plugins\WSA\ARM\" /S /I /F /H /R /K /Y
+xcopy SDK\Client\CSharp\bin\UWP\x86\SharingClient.dll "%HoloToolkitUnityRoot%\Assets\HoloToolkit\Sharing\Plugins\WSA\x86\" /S /I /F /H /R /K /Y
+xcopy SDK\Client\CSharp\bin\UWP\x64\SharingClient.dll "%HoloToolkitUnityRoot%\Assets\HoloToolkit\Sharing\Plugins\WSA\x64\" /S /I /F /H /R /K /Y
 
-xcopy SDK\Server\SharingService.exe "%UnityRoot%\External\HoloToolkit\Sharing\Server\" /S /I /F /H /R /K /Y
+xcopy SDK\Server\SharingService.exe "%HoloToolkitUnityRoot%\External\HoloToolkit\Sharing\Server\" /S /I /F /H /R /K /Y
 
-xcopy SDK\Tools\Profiler\x64\ProfilerX.exe "%UnityRoot%\External\HoloToolkit\Sharing\Tools\Profiler\x64\" /S /I /F /H /R /K /Y
-xcopy SDK\Tools\Profiler\x64\SharingProfiler.dll "%UnityRoot%\External\HoloToolkit\Sharing\Tools\Profiler\x64\" /S /I /F /H /R /K /Y
+xcopy SDK\Tools\Profiler\x64\ProfilerX.exe "%HoloToolkitUnityRoot%\External\HoloToolkit\Sharing\Tools\Profiler\x64\" /S /I /F /H /R /K /Y
+xcopy SDK\Tools\Profiler\x64\SharingProfiler.dll "%HoloToolkitUnityRoot%\External\HoloToolkit\Sharing\Tools\Profiler\x64\" /S /I /F /H /R /K /Y
 
-xcopy SDK\Tools\Profiler\x86\ProfilerX.exe "%UnityRoot%\External\HoloToolkit\Sharing\Tools\Profiler\x86\" /S /I /F /H /R /K /Y
-xcopy SDK\Tools\Profiler\x86\SharingProfiler.dll "%UnityRoot%\External\HoloToolkit\Sharing\Tools\Profiler\x86\" /S /I /F /H /R /K /Y
+xcopy SDK\Tools\Profiler\x86\ProfilerX.exe "%HoloToolkitUnityRoot%\External\HoloToolkit\Sharing\Tools\Profiler\x86\" /S /I /F /H /R /K /Y
+xcopy SDK\Tools\Profiler\x86\SharingProfiler.dll "%HoloToolkitUnityRoot%\External\HoloToolkit\Sharing\Tools\Profiler\x86\" /S /I /F /H /R /K /Y
 
-xcopy SDK\Tools\SessionManager\x64\SessionManager.UI.exe "%UnityRoot%\External\HoloToolkit\Sharing\Tools\SessionManager\x64\" /S /I /F /H /R /K /Y
-xcopy SDK\Tools\SessionManager\x64\SharingClient.dll "%UnityRoot%\External\HoloToolkit\Sharing\Tools\SessionManager\x64\" /S /I /F /H /R /K /Y
+xcopy SDK\Tools\SessionManager\x64\SessionManager.UI.exe "%HoloToolkitUnityRoot%\External\HoloToolkit\Sharing\Tools\SessionManager\x64\" /S /I /F /H /R /K /Y
+xcopy SDK\Tools\SessionManager\x64\SharingClient.dll "%HoloToolkitUnityRoot%\External\HoloToolkit\Sharing\Tools\SessionManager\x64\" /S /I /F /H /R /K /Y
 
-xcopy SDK\Tools\SessionManager\x86\SessionManager.UI.exe "%UnityRoot%\External\HoloToolkit\Sharing\Tools\SessionManager\x86\" /S /I /F /H /R /K /Y
-xcopy SDK\Tools\SessionManager\x86\SharingClient.dll "%UnityRoot%\External\HoloToolkit\Sharing\Tools\SessionManager\x86\" /S /I /F /H /R /K /Y
+xcopy SDK\Tools\SessionManager\x86\SessionManager.UI.exe "%HoloToolkitUnityRoot%\External\HoloToolkit\Sharing\Tools\SessionManager\x86\" /S /I /F /H /R /K /Y
+xcopy SDK\Tools\SessionManager\x86\SharingClient.dll "%HoloToolkitUnityRoot%\External\HoloToolkit\Sharing\Tools\SessionManager\x86\" /S /I /F /H /R /K /Y
 
 exit /b 0
 
 :ErrorNoHoloToolkitUnityTarget
-echo Target path us required. Set the HoloToolkitUnityRoot environment variable or pass the root of the target Unity project as the first argument to this script.
+echo Target path is required. Set the HoloToolkitUnityRoot environment variable or pass the root of the target Unity project as the first argument to this script.
 exit /b 1
 
 :ErrorHoloToolkitUnityTargetInvalid
