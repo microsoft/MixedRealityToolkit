6112aa3ea4c8d8b15eace53e22c6097ec07f62ab
Kurtis Eveleigh <kurtie@microsoft.com>
2017-10-26 14:44:14 -0700

    Updating with Jackson's feedback

--- MotionControllerModel/MotionControllerModel/MotionControllerModel.cpp
+++ MotionControllerModel/MotionControllerModel/MotionControllerModel.cpp
@@ -5,6 +5,7 @@
 
 #include "stdafx.h"
 
+using namespace Concurrency;
 using namespace Microsoft::WRL;
 using namespace Microsoft::WRL::Wrappers;
 using namespace Windows::Foundation;
@@ -12,7 +13,7 @@ using namespace Windows::Perception;
 using namespace Windows::Storage::Streams;
 using namespace Windows::UI::Input::Spatial;
 
-EXTERN_C __declspec(dllexport) BOOL TryGetMotionControllerModel(_In_ UINT32 controllerId, _Out_ UINT32* outputSize, _Outptr_result_bytebuffer_(outputSize) BYTE** outputBuffer)
+EXTERN_C __declspec(dllexport) BOOL TryGetMotionControllerModel(_In_ UINT32 controllerId, _Out_ UINT32& outputSize, _Outptr_result_bytebuffer_(outputSize) BYTE*& outputBuffer)
 {
     ComPtr<ISpatialInteractionManagerInterop> spInteractionInterop;
     ComPtr<IInspectable> spInteractionInteropInspectable;
@@ -27,7 +28,7 @@ EXTERN_C __declspec(dllexport) BOOL TryGetMotionControllerModel(_In_ UINT32 cont
         // Get the current time, in order to create a PerceptionTimestamp. This will be used to get the currently detected spatial sources.
         Windows::Globalization::Calendar^ c = ref new Windows::Globalization::Calendar;
         PerceptionTimestamp^ perceptionTimestamp = PerceptionTimestampHelper::FromHistoricalTargetTime(c->GetDateTime());
-            
+
         // Get the currently detected spatial sources.
         auto sources = spatialInteractionManager->GetDetectedSourcesAtTimestamp(perceptionTimestamp);
 
@@ -37,21 +38,24 @@ EXTERN_C __declspec(dllexport) BOOL TryGetMotionControllerModel(_In_ UINT32 cont
             if (sources->GetAt(i)->Source->Id == controllerId)
             {
                 // Start getting the renderable model stream asynchronously.
-                auto modelOperation = sources->GetAt(i)->Source->Controller->TryGetRenderableModelAsync();
-
-                if (modelOperation == nullptr)
+                task<IRandomAccessStreamWithContentType^> modelOperation;
+                try
+                {
+                    modelOperation = create_task(sources->GetAt(i)->Source->Controller->TryGetRenderableModelAsync());
+                }
+                catch (std::string e)
                 {
                     return FALSE;
                 }
 
-                // Wait until the model call finishes.
-                while (modelOperation->Status == AsyncStatus::Started)
+                task_status status = modelOperation.wait();
+
+                if (status != task_status::completed)
                 {
-                    std::chrono::milliseconds milis(1);
-                    std::this_thread::sleep_for(milis);
+                    return FALSE;
                 }
 
-                auto stream = modelOperation->GetResults();
+                IRandomAccessStreamWithContentType ^stream = modelOperation.get();
 
                 // If the model call failed or the resulting stream is empty, return.
                 if (stream == nullptr || stream->Size == 0)
@@ -61,13 +65,13 @@ EXTERN_C __declspec(dllexport) BOOL TryGetMotionControllerModel(_In_ UINT32 cont
 
                 // Create a buffer from the stream, to read the contents into a byte array for passback to the app.
                 IBuffer^ buffer = ref new Buffer(stream->Size);
-                auto readOperation = stream->ReadAsync(buffer, stream->Size, InputStreamOptions::None);
+                auto readOperation = create_task(stream->ReadAsync(buffer, stream->Size, InputStreamOptions::None));
 
-                // Wait for the stream to be fully read into the buffer.
-                while (readOperation->Status == AsyncStatus::Started)
+                status = readOperation.wait();
+
+                if (status != task_status::completed)
                 {
-                    std::chrono::milliseconds milis(1);
-                    std::this_thread::sleep_for(milis);
+                    return FALSE;
                 }
 
                 // Now, create a DataReader from the buffer, which can then transfer the bytes into a byte array.
@@ -75,8 +79,8 @@ EXTERN_C __declspec(dllexport) BOOL TryGetMotionControllerModel(_In_ UINT32 cont
                 BYTE* pixels = new BYTE[buffer->Length];
                 reader->ReadBytes(Platform::ArrayReference<BYTE>(pixels, buffer->Length));
 
-                *outputBuffer = pixels;
-                *outputSize = buffer->Length;
+                outputBuffer = pixels;
+                outputSize = buffer->Length;
                 return TRUE;
             }
         }
