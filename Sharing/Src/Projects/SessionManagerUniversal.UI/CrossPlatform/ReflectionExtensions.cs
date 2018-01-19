78bca411d1bfc3166f56ca3990814adb46bdc1c8
Kurtis Eveleigh <kurtie@microsoft.com>
2018-01-19 15:46:09 -0800

    ReflectionExtensions `using` cleanup

--- Sharing/Src/Projects/SessionManagerUniversal.UI/CrossPlatform/ReflectionExtensions.cs
+++ Sharing/Src/Projects/SessionManagerUniversal.UI/CrossPlatform/ReflectionExtensions.cs
@@ -4,8 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Diagnostics;
-using System.Text;
 
 public static class ReflectionExtensions
 {
