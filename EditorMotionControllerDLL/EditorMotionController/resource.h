f87cb78ad862cb2c8fb9f57f54012d099e8eea22
Kurtis Eveleigh <kurtie@microsoft.com>
2018-03-26 11:46:12 -0700

    Updating MotionController DLL version and folder name

--- EditorMotionControllerDLL/EditorMotionController/resource.h
+++ EditorMotionControllerDLL/EditorMotionController/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by MotionControllerModel.rc
+// Used by EditorMotionController.rc
 
 // Next default values for new objects
 // 
