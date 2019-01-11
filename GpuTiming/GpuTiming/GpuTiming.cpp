00c2c899d59d0173645c4c63bc8036c7dadb2e8e
NORTHAMERICA\botri <botri@microsoft.com>
2019-01-11 00:27:50 -0800

    Get VRAM use

--- GpuTiming/GpuTiming/GpuTiming.cpp
+++ GpuTiming/GpuTiming/GpuTiming.cpp
@@ -16,9 +16,9 @@
 using namespace std;
 
 class Query;
-typedef shared_ptr<Query> QueryPtr;
-typedef shared_ptr<deque<QueryPtr>> QueryDequePtr;
-typedef shared_ptr<CRITICAL_SECTION> CriticalSectionPtr;
+using QueryPtr = shared_ptr<Query>;
+using QueryDequePtr = shared_ptr<deque<QueryPtr>> ;
+using CriticalSectionPtr = shared_ptr<CRITICAL_SECTION>;
 
 static UnityGfxRenderer s_DeviceType = kUnityGfxRendererNull;
 static IUnityInterfaces* s_UnityInterfaces = nullptr;
@@ -35,7 +35,13 @@ static unordered_map<int, QueryDequePtr> s_ActiveQueries;
 static vector<QueryPtr> s_QueryPool;
 static unordered_map<int, double> s_FrameTimes;
 
-static CRITICAL_SECTION updateSync;
+static CRITICAL_SECTION updateTimingsSync;
+
+#ifdef _WIN32_WINNT_WIN10
+static IDXGIAdapter3* s_Adapter = nullptr;
+static UINT64 s_VramUse = 0;
+static CRITICAL_SECTION updateMemUseSync;
+#endif
 
 enum QueryState
 {
@@ -162,13 +168,16 @@ static void UNITY_INTERFACE_API OnGraphicsDeviceEvent(UnityGfxDeviceEventType ev
 
 static void ReleaseResources();
 
-typedef enum RenderEventType
+enum RenderEventType
 {
 	kRenderEventTypeBeginFrameEventMin = 1000,
 	kRenderEventTypeBeginFrameEventMax = 1999,
 	kRenderEventTypeEndFrameEventMin = 2000,
 	kRenderEventTypeEndFrameEventMax = 2999,
-} RenderEventType;
+#ifdef _WIN32_WINNT_WIN10
+	kRenderEventTypeQueryVideoMemoryEvent = 3000,
+#endif
+};
 
 extern "C" void	UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces* unityInterfaces)
 {
@@ -176,7 +185,7 @@ extern "C" void	UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnit
 	s_Graphics = s_UnityInterfaces->Get<IUnityGraphics>();
 	s_Graphics->RegisterDeviceEventCallback(OnGraphicsDeviceEvent);
 
-	InitializeCriticalSection(&updateSync);
+	InitializeCriticalSection(&updateTimingsSync);
 
 	// Run OnGraphicsDeviceEvent(initialize) manually on plugin load
 	// to not miss the event in case the graphics device is already initialized.
@@ -196,20 +205,35 @@ extern "C" double UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API GetLastFrameGPUTime
 {
 	if (s_Context == nullptr)
 	{
-		return 0;
+		return 0.0;
 	}
 
 	double time = 0;
 
-	EnterCriticalSection(&updateSync);
+	EnterCriticalSection(&updateTimingsSync);
 	auto it = s_FrameTimes.find(eventId);
 	if (it != s_FrameTimes.end())
 	{
 		time = it->second;
 	}
-	LeaveCriticalSection(&updateSync);
+	LeaveCriticalSection(&updateTimingsSync);
 
 	return time;
+}
+
+extern "C" UINT64 UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API GetLastQueriedVramUse(int eventId)
+{
+#ifdef _WIN32_WINNT_WIN10
+	if (s_Adapter == nullptr)
+	{
+		return 0ull;
+	}
+
+	EnterCriticalSection(&updateMemUseSync);
+	return s_VramUse;
+	LeaveCriticalSection(&updateMemUseSync);
+#endif
+	return 0ull;
 }
 
 static void UpdateFrameTime()
@@ -233,9 +257,9 @@ static void UpdateFrameTime()
 			activeQueries->pop_front();
 			s_QueryPool.push_back(q);
 
-			EnterCriticalSection(&updateSync);
+			EnterCriticalSection(&updateTimingsSync);
 			s_FrameTimes[activeQueriesIt->first] = frameTime;
-			LeaveCriticalSection(&updateSync);
+			LeaveCriticalSection(&updateTimingsSync);
 		}
 	}
 }
@@ -246,17 +270,29 @@ static void UNITY_INTERFACE_API OnGraphicsDeviceEvent(UnityGfxDeviceEventType ev
 	if (eventType == kUnityGfxDeviceEventInitialize)
 	{
 		s_DeviceType = s_Graphics->GetRenderer();
-
 		if (s_DeviceType == kUnityGfxRendererD3D11)
 		{
-			// D3D11 is the only API supported.
-
+			// D3D11 is the only API supported
 			IUnityGraphicsD3D11* d3d = s_UnityInterfaces->Get<IUnityGraphicsD3D11>();
 			s_Device = d3d->GetDevice();
 			s_Device->GetImmediateContext(&s_Context);
 
-			// Annotations allow events to be logged so that they are visible in GPUView when profiling.
+			// Annotations allow events to be logged so that they are visible in GPUView when profiling
 			s_Context->QueryInterface(__uuidof(s_Annotation), reinterpret_cast<void**>(&s_Annotation));
+
+#ifdef _WIN32_WINNT_WIN10
+			IDXGIDevice3* device;
+			if (s_Device->QueryInterface(IID_PPV_ARGS(&device)) == S_OK)
+			{
+				IDXGIAdapter* adapter;
+				if (device->GetAdapter(&adapter) == S_OK)
+				{
+					adapter->QueryInterface(IID_PPV_ARGS(&s_Adapter));
+					adapter->Release();
+				}
+				device->Release();
+			}
+#endif
 		}
 	}
 
@@ -269,13 +305,13 @@ static void UNITY_INTERFACE_API OnGraphicsDeviceEvent(UnityGfxDeviceEventType ev
 
 static void UNITY_INTERFACE_API OnRenderEvent(int eventID)
 {
-	// Unknown / unsupported graphics device type? Do nothing
+	// Unknown/unsupported graphics device type; do nothing
 	if (s_Context == nullptr)
 	{
 		return;
 	}
 
-	// Retrieve all available events.
+	// Retrieve all available events
 	UpdateFrameTime();
 
 	if (eventID >= kRenderEventTypeBeginFrameEventMin && eventID <= kRenderEventTypeBeginFrameEventMax)
@@ -317,16 +353,40 @@ static void UNITY_INTERFACE_API OnRenderEvent(int eventID)
 	{
 		int eventId = eventID - kRenderEventTypeEndFrameEventMin;
 		auto activeQueriesIt = s_ActiveQueries.find(eventId);
-
 		if (activeQueriesIt != s_ActiveQueries.end())
 		{
 			activeQueriesIt->second->back()->End();
 		}
 	}
+#ifdef _WIN32_WINNT_WIN10
+	else if (eventID == kRenderEventTypeQueryVideoMemoryEvent && s_Adapter != nullptr)
+	{
+		DXGI_QUERY_VIDEO_MEMORY_INFO vramInfo = {};
+		HRESULT hr = s_Adapter->QueryVideoMemoryInfo(0, DXGI_MEMORY_SEGMENT_GROUP_LOCAL, &vramInfo);
+
+		EnterCriticalSection(&updateMemUseSync);
+		if (hr == S_OK)
+		{
+			s_VramUse = vramInfo.CurrentUsage;
+		}
+		else
+		{
+			s_VramUse = 0ull;
+		}
+		LeaveCriticalSection(&updateMemUseSync);
+	}
+#endif
 }
 
 static void ReleaseResources()
 {
+#ifdef _WIN32_WINNT_WIN10
+	if (s_Adapter != nullptr)
+	{
+		s_Adapter->Release();
+	}
+#endif
+
 	if (s_Annotation != nullptr)
 	{
 		s_Annotation->Release();
@@ -336,10 +396,10 @@ static void ReleaseResources()
 	s_ActiveQueries.clear();
 	s_QueryPool.clear();
 
-	DeleteCriticalSection(&updateSync);
+	DeleteCriticalSection(&updateTimingsSync);
 }
 
-// GetRenderEventFunc, an example function we export which is used to get a rendering event callback function.
+// GetRenderEventFunc, an example function we export which is used to get a rendering event callback function
 extern "C" UnityRenderingEvent UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API GetRenderEventFunc()
 {
 	return OnRenderEvent;
