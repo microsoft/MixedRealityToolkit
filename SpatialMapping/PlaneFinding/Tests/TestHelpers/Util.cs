f6c67ccb9ec2d62ad00ac260f965ec6e02230528
Kurtis <kurtie@microsoft.com>
2019-01-14 18:01:38 -0800

    Fix a few using statements in PlaneFinding test code

--- SpatialMapping/PlaneFinding/Tests/TestHelpers/Util.cs
+++ SpatialMapping/PlaneFinding/Tests/TestHelpers/Util.cs
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using MixedRealityToolkit.SpatialMapping.SpatialProcessing;
 using System.Linq;
 using UnityEngine;
-using HoloToolkit.Unity;
 
 public static class Util
 {
