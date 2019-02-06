e1fe17c60db72bde493f6715aab939c6896e5632
Arthur Tomlin <atomlin@microsoft.com>
2019-02-06 12:33:12 -0800

    ARM64 build target added for Sharing library

--- Sharing/BuildSharingLib.bat
+++ Sharing/BuildSharingLib.bat
@@ -21,6 +21,12 @@ IF NOT %ERRORLEVEL% == 0 goto BuildError
 call MSBuild Src\Solutions\VisualStudio\MixedRealityToolkit.Sharing.sln /p:Configuration=Release;Platform=ARM /m %*
 IF NOT %ERRORLEVEL% == 0 goto BuildError
 
+call MSBuild Src\Solutions\VisualStudio\MixedRealityToolkit.Sharing.sln /p:Configuration=Debug;Platform=ARM64 /m %*
+IF NOT %ERRORLEVEL% == 0 goto BuildError
+
+call MSBuild Src\Solutions\VisualStudio\MixedRealityToolkit.Sharing.sln /p:Configuration=Release;Platform=ARM64 /m %*
+IF NOT %ERRORLEVEL% == 0 goto BuildError
+
 call MSBuild Src\Solutions\VisualStudio\MixedRealityToolkit.Sharing.sln /p:Configuration=Debug;Platform=Win32 /m %*
 IF NOT %ERRORLEVEL% == 0 goto BuildError
 
