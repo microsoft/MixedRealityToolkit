04c21f92ac3464280209879360a33f2d186bf09b
Kurtis <kurtie@microsoft.com>
2020-09-02 16:57:33 -0700

    Update license and namespace

--- SpatialMapping/PlaneFinding/Tests/TestApp/Program.cs
+++ SpatialMapping/PlaneFinding/Tests/TestApp/Program.cs
@@ -1,7 +1,7 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
-using MixedRealityToolkit.SpatialMapping.SpatialProcessing;
+using Microsoft.MixedReality.Toolkit.SpatialAwareness.Processing;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
--- SpatialMapping/PlaneFinding/UnityAddon/PlaneFinding.cs
+++ SpatialMapping/PlaneFinding/UnityAddon/PlaneFinding.cs
@@ -1,12 +1,12 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
-namespace MixedRealityToolkit.SpatialMapping.SpatialProcessing
+namespace Microsoft.MixedReality.Toolkit.SpatialAwareness.Processing
 {
     [StructLayout(LayoutKind.Sequential)]
     public struct OrientedBoundingBox
