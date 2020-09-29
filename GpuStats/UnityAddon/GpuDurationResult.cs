f8bbf36f1d96f4fdd46c365c0561bd22e50f29cf
Kurtis <kurtie@microsoft.com>
2020-09-28 17:25:45 -0700

    Update scripts with latest state of the plug-in

---
+++ GpuStats/UnityAddon/GpuDurationResult.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.MixedReality.Toolkit.Utilities
+{
+    public enum GpuDurationResult
+    {
+        Valid,
+        Disjoint,
+        NotFound,
+    }
+}
