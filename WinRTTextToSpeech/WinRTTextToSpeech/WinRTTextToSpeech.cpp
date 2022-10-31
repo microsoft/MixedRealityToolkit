86584c06d1d7b54a82db2b52d4fb7e33d155bc04
davidkline-ms <davidkl@microsoft.com>
2022-10-31 16:10:53 -0700

    pr feedback: pass 3.1

--- WinRTTextToSpeech/WinRTTextToSpeech/WinRTTextToSpeech.cpp
+++ WinRTTextToSpeech/WinRTTextToSpeech/WinRTTextToSpeech.cpp
@@ -20,7 +20,7 @@ EXTERN_C
 /// </summary>
 /// <param name="phrase">The phrase to be synthesized.</param>
 /// <param name="data">Will receive the audio data in wave format.</param>
-/// <param name="bufferLength">The length of the data being returned.</param>
+/// <param name="dataLength">The length of the data being returned.</param>
 /// <returns>True if the synthesis is successful, or false.</returns>
 DLLEXPORT bool __stdcall TrySynthesizePhrase(
 	const char* phrase,
