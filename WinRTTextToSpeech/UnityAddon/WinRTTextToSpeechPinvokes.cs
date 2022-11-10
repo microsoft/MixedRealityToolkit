f0331fc61c919593904fd534c9fd2f8875406092
David Kline <davidkl@microsoft.com>
2022-11-10 12:25:11 -0800

    add dll to enable using winrt tts api in win32  (#289)
    * wip wrapper for winrt speech synthesis on win32
    
    * in progress tts dll
    
    * update project, fix some obvious errors
    
    * fixed data reading
    
    * a bit of cleanup
    
    * pr feedback: pass 1
    
    * pr feedback: pass 2
    
    * Update WinRTTextToSpeech/WinRTTextToSpeech/WinRTTextToSpeech.cpp
    
    Co-authored-by: MaxWang-MS <68253937+MaxWang-MS@users.noreply.github.com>
    
    * pr feedback: pass 3
    
    * pr feedback: pass 3.1
    
    * improve allocations
    
    * some doc commenting
    
    * add UnityAddon folder
    
    * add meta file for the built dll
    
    * pr feedback: remove x86 build target
    
    * add missing meta file (the .md will be copied during publishing)
    
    * pr feedback: tweak #if placement
    
    Co-authored-by: MaxWang-MS <68253937+MaxWang-MS@users.noreply.github.com>

---
+++ WinRTTextToSpeech/UnityAddon/WinRTTextToSpeechPinvokes.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#if (UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN)                                           
+using System;
+using System.Runtime.InteropServices;
+#endif
+
+namespace Microsoft.MixedReality.Toolkit.Speech.Windows
+{
+    public static class WinRTTextToSpeechPInvokes
+    {
+#if (UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN)                                           
+        [DllImport(
+            "WinRTTextToSpeech.dll",
+            EntryPoint = "TrySynthesizePhrase",
+            CharSet = CharSet.Ansi,
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern bool TrySynthesizePhrase(
+            string phrase,
+            out IntPtr data,
+            out int length);
+
+        [DllImport(
+            "WinRTTextToSpeech.dll",
+            EntryPoint = "FreeSynthesizedData",
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern bool FreeSynthesizedData(IntPtr data);
+#else
+        // This class is unused on non-Windows platforms.
+#endif
+    }
+}
+
+
---
+++ WinRTTextToSpeech/WinRTTextToSpeech/WinRTTextToSpeech.cpp
@@ -0,0 +1,94 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "pch.h"
+
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Media.SpeechSynthesis.h>
+#include <winrt/Windows.Storage.Streams.h>
+
+using namespace winrt;
+using namespace Windows::Media::SpeechSynthesis;
+
+SpeechSynthesizer synthesizer = nullptr;
+
+EXTERN_C
+{
+
+/// <summary>
+/// Synthesizes the provided text into wave format.
+/// </summary>
+/// <param name="phrase">The phrase to be synthesized.</param>
+/// <param name="data">Will receive the audio data in wave format.</param>
+/// <param name="dataLength">The length of the data being returned.</param>
+/// <returns>True if the synthesis is successful, or false.</returns>
+DLLEXPORT bool __stdcall TrySynthesizePhrase(
+	const char* phrase,
+	uint8_t** data,
+	uint32_t& dataLength)
+{
+	// Create the synthesizer on first use.
+	if (!synthesizer)
+	{
+		synthesizer = Windows::Media::SpeechSynthesis::SpeechSynthesizer();
+
+		// Confirm one was created successfully.
+		if (!synthesizer)
+		{
+			*data = nullptr;
+			dataLength = 0;
+			return false;
+		}
+	}
+
+	// Attempt to synthesize the specified phrase.
+	SpeechSynthesisStream speechStream = synthesizer.SynthesizeTextToStreamAsync(to_hstring(phrase)).get();
+	if (!speechStream)
+	{
+		*data = nullptr;
+		dataLength = 0;
+		return false;
+	}
+
+	dataLength = (uint32_t)speechStream.Size();
+	
+	const Windows::Storage::Streams::IInputStream& inputStream = speechStream.GetInputStreamAt(0);
+	speechStream.Close();
+	if (!inputStream)
+	{
+		*data = nullptr;
+		dataLength = 0;
+		return false;
+	}
+
+	Windows::Storage::Streams::DataReader reader = Windows::Storage::Streams::DataReader(inputStream);
+	if (!reader)
+	{
+		inputStream.Close();
+		*data = nullptr;
+		dataLength = 0;
+		return false;
+	}
+
+	// Read the syntesized data and pass it back to the caller.
+	reader.LoadAsync(dataLength).get();
+	*data = new uint8_t[dataLength];
+	reader.ReadBytes(array_view<uint8_t>(*data, *data + dataLength));
+
+	reader.Close();
+	inputStream.Close();
+	
+	return true;
+}
+
+/// <summary>
+/// Frees the memory allocated during synthesis.
+/// </summary>
+/// <param name="data">Pointer to the data to be freed.</param>
+DLLEXPORT void __stdcall FreeSynthesizedData(uint8_t* data)
+{
+	if (!data) { return; }
+	delete[] data;
+}
+
+}
